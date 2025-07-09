1cecdf1e1381292f08311cfd66bfc6481e3467c9
RAJAT0444 <rajatkumar6072@gmail.com>
2025-07-09 19:31:53 +0530

    Update homepage for redeploy

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -74,3 +74,24 @@ export const authOptions = {
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import NextAuth from "next-auth";
+// import { authOptions } from "./options";
+
+// const handler = NextAuth(authOptions);
+
+// export const GET = handler;
+// export const POST = handler;
+
